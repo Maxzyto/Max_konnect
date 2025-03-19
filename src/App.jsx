5f763f8f5d477b8be4d8d6d801815c2fa6bf4a70
Maxzyto <adaaziabugrimaxwell@gmail.com>
2025-03-19 23:27:59 +0000

    All cmponent added ready for building

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
+import Dashboard from './pages/Dashboard'
 
 function App() {
 
   return (
     <>
-      <h1 className='bg-blue-400'>Welcome to Max Konnect</h1>
+     
+      <Dashboard />
     </>
   )
 }
