8273e363f0db83ea2532c9e90b83942417882be8
Maxzyto <adaaziabugrimaxwell@gmail.com>
2025-03-19 23:35:37 +0000

    Dashboard pages added

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Upload from '../components/Upload'
+import History from '../components/History'
+import Receipt from '../components/Receipt'
+import Chat from '../components/Chat'
 
 const Dashboard = () => {
   return (
     <div>
-      <h1 className='color-black bg-blue-400'>Welcome User</h1>
+      <h1 className='color-black bg-blue-400 text-3xl text-bold'>Welcome to Max Konnect</h1>
+      <Upload />
+      <History />
+      <Receipt />
+      <Chat />
     </div>
   )
 }
