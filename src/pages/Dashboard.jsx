a4adc8be473a67c34d53a56bbca1ec3c33255e5a
Maxzyto <adaaziabugrimaxwell@gmail.com>
2025-03-30 21:03:43 +0000

    navbar complete

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,11 @@
 import Navbar from '../components/Navbar';
 
 const Dashboard = () => {
-  const onLogout = () => {
-    console.log("User logged out");
-  };
+
 
   return (
-    <div className="p-4 h-100 w-100 bg-cover ps-10 pt-20 pl-52 overflow-hidden" >
-      <Navbar user={{ name: "Guest" }} onLogout={onLogout} />
+    <div className="p-4 h-100 w-100 bg-cover ps-10 pt-20 pl-30 overflow-hidden" >
+      {/* <Navbar user={{ name: "Guest" }} onLogout={onLogout} /> */}
       <h1 className="color-black text-6xl text-bold w-screen ">Welcome to Max Konnect</h1>
     </div>
   );
