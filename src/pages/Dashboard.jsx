fc60245dd9d472c98009033bf3b4fff57730a3ce
Maxzyto <adaaziabugrimaxwell@gmail.com>
2025-03-31 20:49:54 +0000

    more dashboard styles

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,18 +5,27 @@ const Dashboard = () => {
 
   return (
     <div
-      className="p-4 h-full w-full bg-cover overflow-hidden flex flex-col items-center justify-center"
+      className="p-4 h-screen w-screen scroll-m-0 bg-transparent overflow-clip flex flex-col items-center justify-center"
       style={{
         backgroundImage: "url('/pngwing.com.png')",
         backgroundSize: "cover",
         backgroundPosition: "center",
-        backgroundRepeat: "repeat",
+        backgroundRepeat: "no-repeat",
         backgroundColor: "rgba(0, 0, 0, 0.5)",
         backgroundBlendMode: "overlay",
+        overflow: "hidden",
+        position: "fixed",
+        zIndex: 0,
+        width: "100%",
+        height: "100%",
+        top: 0,
+        left: 0,
+        display: "flex",
+        justifyContent: "center",
+
       }}
     >
-      <h1 className="text-white text-6xl font-bold text-center mt-8">PRINTING <br/> CREATIVITY <br/> SOLUTION</h1>
-      
+      <h1 className="text-white text-6xl font-bold p-20 text-center mt-6">PRINTING <br/> CREATIVITY <br/> SOLUTION</h1>
     </div>
   );
 }
